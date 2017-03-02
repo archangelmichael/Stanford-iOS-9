cbff483a8f7d27bbddfb69bc3e527abe44274a08
Radi <radishikerov@yahoo.com>
2017-03-02 11:11:43 +0200

    Change facial expression and implement inspectables

--- Drawing/Drawing/FaceView.swift
+++ Drawing/Drawing/FaceView.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
+@IBDesignable
 class FaceView: UIView {
-    let scale : CGFloat = 0.8
-    let lineWidth : CGFloat = 2.0
-    let eyeRadius : CGFloat = 10.0
-
+    
+    private let angleMultiplier : CGFloat = 1/sqrt(2)
+    
+    @IBInspectable
+    var scale : CGFloat = 0.8 { didSet { setNeedsDisplay() } }
+    
+    @IBInspectable
+    var lineWidth : CGFloat = 2.0 { didSet { setNeedsDisplay() } }
+    @IBInspectable
+    var eyeRadiusOffset : CGFloat = 0.3{ didSet { setNeedsDisplay() } }
+    @IBInspectable
+    var eyeCenterOffset : CGFloat = 0.3{ didSet { setNeedsDisplay() } }
+    private var mouthOffset : CGFloat = 0.5
+    
+    @IBInspectable
+    var fillColor : UIColor = UIColor.green{ didSet { setNeedsDisplay() } }
+    @IBInspectable
+    var strokeColor : UIColor = UIColor.blue{ didSet { setNeedsDisplay() } }
+    @IBInspectable
+    var mouthState : Int = 0 { didSet { setNeedsDisplay() } } // 1 - smile, 0 - normal, -1 - fraun
+    
     private var faceRadius : CGFloat {
         return min(bounds.size.height, bounds.size.width)/2 * scale
+    }
+    
+    private var eyeRadius : CGFloat {
+        return eyeCenterDistance * eyeRadiusOffset
+    }
+    
+    private var eyeCenterDistance : CGFloat {
+        return faceRadius * eyeCenterOffset
     }
     
     private var faceCenter : CGPoint {
@@ -23,10 +49,41 @@ class FaceView: UIView {
     
     private func pathForCircleAt(center: CGPoint, radius: CGFloat) -> UIBezierPath {
         let path = UIBezierPath(arcCenter: center,
-                     radius: radius,
-                     startAngle: CGFloat(0) ,
-                     endAngle: CGFloat(2 * M_PI),
-                     clockwise: true)
+                                radius: radius,
+                                startAngle: CGFloat(0) ,
+                                endAngle: CGFloat(2 * M_PI),
+                                clockwise: true)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private func pathForMouthAt() -> UIBezierPath {
+        var path : UIBezierPath
+        switch mouthState {
+        case 1: // Smile
+            path = UIBezierPath(arcCenter: faceCenter,
+                                radius: faceRadius * mouthOffset,
+                                startAngle: CGFloat(1/4 * M_PI),
+                                endAngle: CGFloat(3/4 * M_PI),
+                                clockwise: true)
+            break
+        case -1: // Fraun
+            path = UIBezierPath(arcCenter: CGPoint(x: faceCenter.x,
+                                                   y: faceCenter.y + faceRadius),
+                                radius: faceRadius * mouthOffset,
+                                startAngle: CGFloat(-1/4 * M_PI),
+                                endAngle: CGFloat(-3/4 * M_PI),
+                                clockwise: false)
+            break
+        default: // Normal
+            path = UIBezierPath()
+            path.move(to: CGPoint(x: faceCenter.x - angleMultiplier * faceRadius * mouthOffset,
+                                  y: faceCenter.y + angleMultiplier * faceRadius * mouthOffset))
+            path.addLine(to: CGPoint(x: faceCenter.x + angleMultiplier * faceRadius * mouthOffset,
+                                     y: faceCenter.y + angleMultiplier * faceRadius * mouthOffset))
+            break
+        }
+        
         path.lineWidth = lineWidth
         return path
     }
@@ -34,33 +91,27 @@ class FaceView: UIView {
     override func draw(_ rect: CGRect) {
         
         let face = self.pathForCircleAt(center: faceCenter, radius: faceRadius)
-        UIColor.green.setFill()
+        fillColor.setFill()
         face.fill()
         
-        
-        let mouth = UIBezierPath(arcCenter: faceCenter,
-                                 radius: faceRadius * 2 / 3,
-                                 startAngle: CGFloat(1/4 * M_PI),
-                                 endAngle: CGFloat(3/4 * M_PI),
-                                 clockwise: true)
-        mouth.lineWidth = 2.0
-        UIColor.blue.setStroke()
+        let mouth = pathForMouthAt()
+        strokeColor.setStroke()
         mouth.stroke()
         
-        let leftEyeCenter = CGPoint(x: 2/3 * faceCenter.x,
-                                    y: 2/3 * faceCenter.y)
+        let leftEyeCenter = CGPoint(x: faceCenter.x - angleMultiplier * eyeCenterDistance,
+                                    y: faceCenter.y - angleMultiplier * eyeCenterDistance)
         let leftEye = self.pathForCircleAt(center: leftEyeCenter, radius: eyeRadius)
-        UIColor.blue.setStroke()
+        strokeColor.setStroke()
         leftEye.stroke()
         
-        let rightEyeCenter = CGPoint(x: 4/3 * faceCenter.x,
-                                    y: 2/3 * faceCenter.y)
+        let rightEyeCenter = CGPoint(x: faceCenter.x + angleMultiplier * eyeCenterDistance,
+                                     y: faceCenter.y - angleMultiplier * eyeCenterDistance)
         let rightEye =  self.pathForCircleAt(center: rightEyeCenter, radius: eyeRadius)
-        UIColor.blue.setStroke()
+        strokeColor.setStroke()
         rightEye.stroke()
     }
     
-
+    
     static func drawFace(vc : UIViewController) {
         let width = vc.view.frame.size.width
         let height = vc.view.frame.size.height
